03cd7c91c389f5e68198f77001260ff0e530c3a6
Ruddy <ruddy391@gmail.com>
2026-07-19 00:40:08 +0200

    Add calculator component to main layout

--- src/app/layout/main-layout/main-layout.ts
+++ src/app/layout/main-layout/main-layout.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Calculator } from '../../features/calculator/calculator';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [],
+  standalone: true,
+  imports: [Calculator],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.css',
 })
