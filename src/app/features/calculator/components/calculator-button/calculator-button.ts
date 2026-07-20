3ab88a19bef8175dfa1a749e5ba0435a8bde0e94
Ruddy <ruddy391@gmail.com>
2026-07-20 09:04:13 +0200

    feat: implement calculator button component with label input

--- src/app/features/calculator/components/calculator-button/calculator-button.ts
+++ src/app/features/calculator/components/calculator-button/calculator-button.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-calculator-button',
@@ -6,4 +6,6 @@ import { Component } from '@angular/core';
   templateUrl: './calculator-button.html',
   styleUrl: './calculator-button.css',
 })
-export class CalculatorButton {}
+export class CalculatorButton {
+ readonly label = input.required<string>();
+}
