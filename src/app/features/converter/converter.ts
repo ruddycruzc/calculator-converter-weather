a5f131c23aeba3c9e6cdab7cfd1c0fda9b9738fb
Ruddy <ruddy391@gmail.com>
2026-07-23 14:20:48 +0200

    feat(converter): implement currency conversion logic and UI components

--- src/app/features/converter/converter.ts
+++ src/app/features/converter/converter.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
 import { CurrencyService } from './services/currency.service';
 
 @Component({
@@ -16,8 +16,54 @@ readonly rates = this.currencyService.rates;
 readonly loading = this.currencyService.loading;
 readonly error = this.currencyService.error;
 
+  amount = signal(1);
+  fromCurrency = signal('EUR');
+  toCurrency = signal('USD');
+  result = signal<number | null>(null);
+
+  readonly currencies = computed(() => {
+
+    const response = this.rates();
+
+    if (!response) {
+      return [];
+    }
+
+    return Object.keys(response.rates);
+
+  });
+
 ngOnInit(): void {
   this.currencyService.loadRates();
 }
 
+onAmountChange(event: Event): void {
+
+  const value = Number(
+    (event.target as HTMLInputElement).value
+  );
+
+  this.amount.set(value);
+
+}
+
+ onFromCurrencyChange(event: Event): void {
+
+  const value =
+    (event.target as HTMLSelectElement).value;
+
+  this.fromCurrency.set(value);
+
+}
+
+  onToCurrencyChange(event: Event): void {
+
+  const value =
+    (event.target as HTMLSelectElement).value;
+
+  this.toCurrency.set(value);
+
+}
+
+  convert(): void {}
 }
